ac59d21995fc0f635e59288bbc644d1735a6d091
Aleksandr <aleksandr.v.dikarev@gmail.com>
2018-06-29 23:45:28 +0300

    Add files via upload

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Underwater communiation & navigation laboratory, LLC")]
 [assembly: AssemblyProduct("UCNLNMEA")]
-[assembly: AssemblyCopyright("© 2015-2017, Underwater communiation & navigation laboratory, LLC")]
+[assembly: AssemblyCopyright("© 2015, Underwater communiation & navigation laboratory, LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
